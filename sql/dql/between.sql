dbcf776c9e3e76bdb936aff66c7187c36351c883
Gabriel Frigo <gfrigo.sena@gmail.com>
2025-07-23 22:21:43 -0300

    feat(dql): between and not between

--- sql/dql/between.sql
+++ sql/dql/between.sql
@@ -1,7 +1,14 @@
--- BETWEEN e NOT BETWEEN
-SELECT * FROM products
+-- Definição: Filtragem de dados em um determinado intervalo
+-- Obs: Evita usar diversos operadores lógicos ao mesmo tempo. 
+
+-- Obtém dados que estejam alfabeticamente ENTRE 'C' e 'M'.
 WHERE product_name BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni'
 ORDER BY product_name;
 
+-- Obtém dados que estejam ENTRE as datas.
 SELECT * FROM orders
 WHERE order_date BETWEEN '07/04/1996' AND '07/09/1996';
+
+-- Obtém dados que estejam ENTRE os valores inteiros.
+SELECT * FROM order_details
+WHERE unit_price BETWEEN 10 and 20;
