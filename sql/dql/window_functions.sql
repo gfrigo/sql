fe0f7ea184a454eb2c87b225ee29cba554580b3e
Gabriel Frigo <gfrigo.sena@gmail.com>
2025-07-25 16:09:40 -0300

    feat(dql): window functions advantages

--- sql/dql/window_functions.sql
+++ sql/dql/window_functions.sql
@@ -15,3 +15,14 @@ SELECT order_id,
    SUM(unit_price * quantity) OVER (PARTITION BY order_id) AS total_price
 FROM order_details
 ORDER BY order_id ASC;
+
+/*
+Vantagens em usar Window Functions
+1- Preservação de Dados Detalhados: Ao contrário do GROUP BY, que agrega e reduz os
+dados a uma única linha por grupo, as funções de janela mantêm cada linha individual
+do conjunto de dados original visível. Isso é útil para análises detalhadas onde
+ você precisa ver tanto os valores agregados quanto os dados de linha individual.
+
+2- Flexibilidade: Você pode calcular múltiplas métricas de agregação em diferentes
+partições dentro da mesma consulta sem múltiplas passagens pelos dados ou subconsultas complexas.
+*/
