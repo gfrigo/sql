5c9fa8dcdac3b78cb9db526182546e06beae6b60
Gabriel Frigo <gfrigo.sena@gmail.com>
2025-07-26 09:27:21 -0300

    feat(ddl): update

---
+++ sql/ddl/update.sql
@@ -0,0 +1,11 @@
+/*
+Definição: Atualiza uma linhda de dados em colunas específicas mediante filtro
+com WHERE.
+
+IMPORTANTE: A execução do update sem WHERER, atribuirá o valor em TODAS as linhas.
+*/
+
+UPDATE customer_feedback
+SET feedback_date = '2025-07-26',
+    rating = 1
+WHERE customer_id = 'ALFKI' AND order_id = 10248;
