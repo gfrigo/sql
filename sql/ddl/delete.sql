29c6010828a00401e5de530e7d5b6d84e4b78b08
Gabriel Frigo <gfrigo.sena@gmail.com>
2025-07-26 09:36:01 -0300

    fix(ddl): remove dml queries

--- sql/ddl/delete.sql
+++
@@ -1,8 +0,0 @@
-/*
-Definição: Faz a deleção de uma ou mais linhas de dados mediante filtro com WHERE.
-
-IMPORTANTE: A execução do DELETE sem WHERE, excluirá TODOS os dados da tabela.
-*/
-
-DELETE FROM customer_feedback
-WHERE rating = 1;
